0fc4857a5e19aa853b287ec8994a5320bdab8520
Chichambola <zhopacpu4koy@yandex.ru>
2025-10-09 19:34:22 +0700

    Adding waypoints

--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -1,18 +1,43 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
+using Random = UnityEngine.Random;
 using UnityEngine;
 
 public class Target : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private List<Waypoint> _waypoints;
+
+    private float _speed = 3f;
+    private Coroutine _coroutine;
+
+    private void Awake()
     {
-        
+        _coroutine = StartCoroutine(Moving());
     }
 
-    // Update is called once per frame
-    void Update()
+    private IEnumerator Moving()
     {
+        while (enabled)
+        {
+            Vector3 waypointPosition = GetWaypointPosition();
         
+            while (transform.position != waypointPosition)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, waypointPosition, Time.deltaTime * _speed);
+            
+                yield return null;
+            }   
+        }
+    }
+
+    private Vector3 GetWaypointPosition()
+    {
+        int firstIndex = 0;
+        
+        var waypoint = _waypoints[Random.Range(firstIndex, _waypoints.Count)];
+        
+        return waypoint.transform.position;
     }
 }
